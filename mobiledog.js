08c2c84e50e5e3413d5caf876e57ffdc8e5c8694
dialixx <105409038+dialixx@users.noreply.github.com>
2022-05-12 12:33:54 -0500

    script issue 2:

---
+++ mobiledog.js
@@ -0,0 +1,10 @@
+function desktopbutitsnotnameddesktop() {
+	
+	if (x.matches) {
+		window.location.replace("https://dialixx.github.io/hellscape_web/");
+	}
+
+}
+
+var x = window.matchMedia("(min-width: 700px)");
+desktopbutitsnotnameddesktop(x);
