cd02a6fd7a506a5a20f09daddcc7524bb33dfb1f
dialixx <105409038+dialixx@users.noreply.github.com>
2022-05-14 11:32:48 -0500

    Update sidewaysdog.js

--- sidewaysdog.js
+++ sidewaysdog.js
@@ -93,3 +93,119 @@ function dogclick() {
 	}
 	
 }
+
+let dogstick = 0;
+let dogroller = 0;
+let dogdrop = 0;
+let dogdoin = 0;
+let uprightdog = 0;
+let sidewayscanine = 0;
+let sidewaysdog = 0;
+
+let sidewaysdogmulti = (sidewaysdog * 137849526);
+
+let dogpersecond = 0;
+
+
+function dps1() {
+		
+	dogcount += dogpersecond;
+	document.getElementById("numdog").innerHTML = dogcount + " dogs";
+		
+}
+
+setInterval(dps1, 1000);
+
+
+function purchasestick() {
+	
+	if(dogcount >= 10) {
+		dogpersecond += 1;
+		dogcount -= 10;
+		dogstick += 1;
+		
+		document.getElementById("dograte").innerHTML = dogpersecond + " dogs per second";
+		document.getElementById("numdog").innerHTML = dogcount + " dogs";
+		
+		document.getElementById("buildingcount").innerHTML = "you have...<br>" + dogstick + " dog sticks<br>" + dogroller + " dog rollers<br>" + dogdrop + " dog drops<br>" + dogdoin + " dog doins<br>" + uprightdog + " upright dogs<br>" + sidewayscanine + " sideways dogs<br>" + sidewaysdog + " sideways dogs";
+	}
+	
+}
+
+function purchaseroller() {
+	if(dogcount >= 50) {
+		dogpersecond += 3;
+		dogcount -= 50;
+		dogroller += 1;
+		
+		document.getElementById("dograte").innerHTML = dogpersecond + " dogs per second";
+		document.getElementById("numdog").innerHTML = dogcount + " dogs";
+		
+		document.getElementById("buildingcount").innerHTML = "you have...<br>" + dogstick + " dog sticks<br>" + dogroller + " dog rollers<br>" + dogdrop + " dog drops<br>" + dogdoin + " dog doins<br>" + uprightdog + " upright dogs<br>" + sidewayscanine + " sideways dogs<br>" + sidewaysdog + " sideways dogs";
+	}
+}
+
+function purchasedrop() {
+	if(dogcount >= 500) {
+		dogpersecond += 40;
+		dogcount -= 500;
+		dogdrop += 1;
+		
+		document.getElementById("dograte").innerHTML = dogpersecond + " dogs per second";
+		document.getElementById("numdog").innerHTML = dogcount + " dogs";
+		
+		document.getElementById("buildingcount").innerHTML = "you have...<br>" + dogstick + " dog sticks<br>" + dogroller + " dog rollers<br>" + dogdrop + " dog drops<br>" + dogdoin + " dog doins<br>" + uprightdog + " upright dogs<br>" + sidewayscanine + " sideways dogs<br>" + sidewaysdog + " sideways dogs";
+	}
+}
+
+function purchasedoin() {
+	if(dogcount >= 1500) {
+		dogpersecond += 100;
+		dogcount -= 1500;
+		dogdoin += 1;
+		
+		document.getElementById("dograte").innerHTML = dogpersecond + " dogs per second";
+		document.getElementById("numdog").innerHTML = dogcount + " dogs";
+		
+		document.getElementById("buildingcount").innerHTML = "you have...<br>" + dogstick + " dog sticks<br>" + dogroller + " dog rollers<br>" + dogdrop + " dog drops<br>" + dogdoin + " dog doins<br>" + uprightdog + " upright dogs<br>" + sidewayscanine + " sideways dogs<br>" + sidewaysdog + " sideways dogs";
+	}
+}
+
+function purchaseupright() {
+	if(dogcount >= 8000) {
+		dogpersecond += 1000;
+		dogcount -= 8000;
+		uprightdog += 1;
+		
+		document.getElementById("dograte").innerHTML = dogpersecond + " dogs per second";
+		document.getElementById("numdog").innerHTML = dogcount + " dogs";
+		
+		document.getElementById("buildingcount").innerHTML = "you have...<br>" + dogstick + " dog sticks<br>" + dogroller + " dog rollers<br>" + dogdrop + " dog drops<br>" + dogdoin + " dog doins<br>" + uprightdog + " upright dogs<br>" + sidewayscanine + " sideways dogs<br>" + sidewaysdog + " sideways dogs";
+	}
+}
+
+function purchasecanine() {
+	if(dogcount >= 30000) {
+		dogpersecond += 0;
+		dogcount -= 30000;
+		sidewayscanine += 1;
+		
+		document.getElementById("dograte").innerHTML = dogpersecond + " dogs per second";
+		document.getElementById("numdog").innerHTML = dogcount + " dogs";
+		
+		document.getElementById("buildingcount").innerHTML = "you have...<br>" + dogstick + " dog sticks<br>" + dogroller + " dog rollers<br>" + dogdrop + " dog drops<br>" + dogdoin + " dog doins<br>" + uprightdog + " upright dogs<br>" + sidewayscanine + " sideways dogs<br>" + sidewaysdog + " sideways dogs";
+	}
+}
+
+function purchasesideways() {
+	if(dogcount >= 100000) {
+		dogpersecond += 1278394123457092873;
+		dogcount -= 100000;
+		sidewaysdog += 1;
+		
+		document.getElementById("dograte").innerHTML = "lots of dogs per second";
+		document.getElementById("numdog").innerHTML = dogcount + " dogs";
+		
+		document.getElementById("buildingcount").innerHTML = "you have...<br>" + dogstick + " dog sticks<br>" + dogroller + " dog rollers<br>" + dogdrop + " dog drops<br>" + dogdoin + " dog doins<br>" + uprightdog + " upright dogs<br>" + sidewayscanine + " sideways dogs<br>" + sidewaysdog + " sideways dogs";
+	}
+}
