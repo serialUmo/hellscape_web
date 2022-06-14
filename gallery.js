830a88a0dcd2cd378a33625a8e73567d0d37da54
dialixx <105409038+dialixx@users.noreply.github.com>
2022-06-14 00:33:11 -0500

    gallery html css js

---
+++ gallery.js
@@ -0,0 +1,8 @@
+function changeartimg(a) {
+		document.getElementById("artimgfull").src = a;
+		document.getElementById("artdisplay").style.display = "block";
+	}
+	
+	function artdisplayclose() {
+		document.getElementById("artdisplay").style.display = "none";
+	}
